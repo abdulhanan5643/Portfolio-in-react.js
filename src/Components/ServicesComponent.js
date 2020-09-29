2725ee9f34fe4c3e1dea2eee0eddc4792f89071e
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-09-29 23:38:43 +0500

    services part 2

--- src/Components/ServicesComponent.js
+++ src/Components/ServicesComponent.js
@@ -1,11 +1,27 @@
-import  React, {Component} from 'react';
+import { PhoneIphone } from '@material-ui/icons';
+import React, { Component } from 'react';
 import '../css/Services.css';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 
-class Services extends Component{
-    render(){
+class Services extends Component {
+    render() {
         return <div className="services">
             <h3>Services</h3>
             <h2>What we offer?</h2>
+            <div className="row">
+                <div>
+                    <span><PhoneIphoneIcon /></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+            </div>
         </div>
     }
 };
