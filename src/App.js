f9b302373a550fb55be186b2f4a2e359671e2479
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-09-29 22:51:31 +0500

    services part 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/HeaderComponent';
 import Navigation from './Components/NavigationComponent';
+import Services from './Components/ServicesComponent';
 
 function App() {
   return (
     <div className="App">
       <Navigation logoTitle="Abdul Hanan Asif" />
       <Header title="Portfolio" button="Find out more" />
+      <Services />
     </div>
   );
 }
--- src/Components/ServicesComponent.js
+++ src/Components/ServicesComponent.js
@@ -1,7 +1,13 @@
 import  React, {Component} from 'react';
+import '../css/Services.css';
 
 class Services extends Component{
-
+    render(){
+        return <div className="services">
+            <h3>Services</h3>
+            <h2>What we offer?</h2>
+        </div>
+    }
 };
 
 export default Services;
