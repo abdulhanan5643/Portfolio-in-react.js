634a59acb99383008de3da98517fbf411e35caec
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-09-30 16:15:50 +0500

    services part 3

--- src/Components/ServicesComponent.js
+++ src/Components/ServicesComponent.js
@@ -1,7 +1,9 @@
-import { PhoneIphone } from '@material-ui/icons';
 import React, { Component } from 'react';
 import '../css/Services.css';
-import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+import CreateIcon from '@material-ui/icons/Create';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import HelpIcon from '@material-ui/icons/Help';
 
 class Services extends Component {
     render() {
@@ -10,16 +12,24 @@ class Services extends Component {
             <h2>What we offer?</h2>
             <div className="row">
                 <div>
-                    <span><PhoneIphoneIcon /></span>
+                    <span><PhoneAndroidIcon /></span>
+                    <h4>Responsive</h4>
+                    <p>Looks great on any screen size</p>
                 </div>
                 <div>
-                    <span></span>
+                    <span><CreateIcon /></span>
+                    <h4>Redesigned</h4>
+                    <p>Freshly redesigned for Bootstrap</p>
                 </div>
                 <div>
-                    <span></span>
+                    <span><ThumbUpIcon /></span>
+                    <h4>Favourited</h4>
+                    <p>Millions of users Start Bootstrap!</p>
                 </div>
                 <div>
-                    <span></span>
+                    <span><HelpIcon /></span>
+                    <h4>Question</h4>
+                    <p>I mushtache you a question...</p>
                 </div>
             </div>
         </div>
